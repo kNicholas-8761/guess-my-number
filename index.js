7040258685c1cdf3885fdea3a0e4466f0665030f
Kevin Brown <kevinbrown@192-168-1-101.tpgi.com.au>
2023-09-05 20:28:43 +1000

    check if guess  is too high or too low

--- index.js
+++ index.js
@@ -11,5 +11,9 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".message").textContent = "⛔️ No Number ";
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "Correct Number!";
+  } else if (guess > secretNumber) {
+    document.querySelector(".message").textContent = "📈 Too high!";
+  } else if (guess < secretNumber) {
+    document.querySelector(".message").textContent = "📉 Too low!";
   }
 });
