4279a6610c879da8d0bb2ac23bfb41dba24b1106
Kevin Brown <kevinbrown@192-168-1-101.tpgi.com.au>
2023-09-05 20:55:12 +1000

    adjust score when guess is wrong.

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 "use strict";
 
+let score = 20;
+
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 document.querySelector(".number").textContent = secretNumber;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
-  console.log(typeof guess, guess);
 
   if (!guess) {
     document.querySelector(".message").textContent = "⛔️ No Number ";
@@ -13,7 +14,11 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".message").textContent = "Correct Number!";
   } else if (guess > secretNumber) {
     document.querySelector(".message").textContent = "📈 Too high!";
+    score--;
+    document.querySelector(".score").textContent = score;
   } else if (guess < secretNumber) {
     document.querySelector(".message").textContent = "📉 Too low!";
+    score--;
+    document.querySelector(".score").textContent = score;
   }
 });
