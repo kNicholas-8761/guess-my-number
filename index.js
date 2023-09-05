75bf6383eefa40c0bf52d997564eb567b8ae48e4
Kevin Brown <kevinbrown@192-168-1-101.tpgi.com.au>
2023-09-06 09:14:54 +1000

    change background when player wins.

--- index.js
+++ index.js
@@ -8,17 +8,33 @@ document.querySelector(".number").textContent = secretNumber;
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
 
+  // When there is no input
   if (!guess) {
     document.querySelector(".message").textContent = "⛔️ No Number ";
+
+    // When player wins
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "Correct Number!";
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    // When guess is too high
   } else if (guess > secretNumber) {
-    document.querySelector(".message").textContent = "📈 Too high!";
-    score--;
-    document.querySelector(".score").textContent = score;
+    if (score > 1) {
+      document.querySelector(".message").textContent = "📈 Too high!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "🥲 You lost the game";
+      document.querySelector(".score").textContent = 0;
+    }
+    // When guess is too low
   } else if (guess < secretNumber) {
-    document.querySelector(".message").textContent = "📉 Too low!";
-    score--;
-    document.querySelector(".score").textContent = score;
+    if (score > 1) {
+      document.querySelector(".message").textContent = "📉 Too low!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "🥲 You lost the game";
+      document.querySelector(".score").textContent = 0;
+    }
   }
 });
