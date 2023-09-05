360faa026e38a6e1ae48a5197b29d7bc88bb3d01
Kevin Brown <kevinbrown@192-168-1-101.tpgi.com.au>
2023-09-05 19:18:38 +1000

    check if guess is equal to secret number

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 "use strict";
 
-const number = Math.trunc(Math.random() * 20) + 1;
-document.querySelector(".number").textContent = number;
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(".number").textContent = secretNumber;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -9,5 +9,7 @@ document.querySelector(".check").addEventListener("click", function () {
 
   if (!guess) {
     document.querySelector(".message").textContent = "⛔️ No Number ";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "Correct Number!";
   }
 });
